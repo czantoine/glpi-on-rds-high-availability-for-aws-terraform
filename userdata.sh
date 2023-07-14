6525f040cf2791b497a2c0513d552267d44af9f3
Antoine Cichowicz <ac11f@free.fr>
2023-07-14 11:40:59 +0200

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 sudo apt-get update
 sudo apt-get install awscli -y
-aws s3 cp s3://sandbox-demo-bucket-s3/script.sh ./script.sh
+aws s3 cp s3://<bucket-name>/script.sh ./script.sh
 sudo bash script.sh
